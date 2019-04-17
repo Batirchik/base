937ab8cae8f3d06de149ea324c9d6fbaa7c0b4d9
Aleksandr Parkhomenko <aleksander.parkhomenko@macys.com>
2019-04-17 16:11:50 +0300

    Move the task to README

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# base
+# 'base' project
+
+# INPUT:
+Service listens on the same host, public IP. Accepts params by GET/POST.
+API token expires in 5mins.
+If task is not started, returns 404 code with "Not found" message.
+When task completed successfully - returns 200 code with some log output containing "RESULT: INTEGER" line.
+Otherwise - 500 code with log output and "ERROR: STRING" line.
+We need to start task, control its result and restart if failed or every 60 seconds.
+The process must work on permanent basis in background.
+
+
+# TODO:
+Improve the base solution.
+
+# OBJECTIVE:
+1) Universalize;
+2) Optimize;
+3) Secure.
+
+Please make sent a pull request or link to your repository with the solution.
+
+Thanks
--- task_runner.sh
+++ task_runner.sh
@@ -4,20 +4,6 @@ USER="test"
 PASS="test"
 SERVICE_ENDPOINT="10.0.0.255:9999"
 
-# INPUT:
-# Service listens on the same host, public IP. Accepts params by GET/POST.
-# API token expires in 5mins.
-# If task is not started, returns 404 code with "Not found" message.
-# When task completed successfully - returns 200 code with some log output containing "RESULT: INTEGER" line.
-# Otherwise - 500 code with log output and "ERROR: STRING" line.
-# We need to start task, control its result and restart if failed or every 60 seconds. 
-# The process must work on permanent basis in background.
-
-# OBJECTIVE:
-# 1) Universalize;
-# 2) Optimize;
-# 3) Secure.
-
 while true; do
 curl http://$SERVICE_ENDPOINT/apitoken?user=$USER&pass=$PASS > api_token
 API_TOKEN=`cat api_token`
